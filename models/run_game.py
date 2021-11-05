69bd1f62bfba0dfc8c4d37ae4ec3f604c0432355
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-05 14:51:32 +0000

    adds list_players

--- models/run_game.py
+++ models/run_game.py
@@ -6,3 +6,9 @@ def run_game(hand1, hand2):
     player2 = Player("Player 2", hand2)
     game = Game(player1, player2)
     return game.play()
+
+def list_players(hand1, hand2):
+    player1 = Player("Player 1", hand1)
+    player2 = Player("Player 2", hand2)
+    players = [player1, player2]
+    return players
