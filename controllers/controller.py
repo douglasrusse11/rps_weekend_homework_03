39b8c24c2607c09a22565687c89e2e9cf7bbd9f5
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-05 18:14:33 +0000

    modifies /hand1/hand2 route to validate input

--- controllers/controller.py
+++ controllers/controller.py
@@ -10,7 +10,9 @@ def index():
 def rps(hand1, hand2):
     winner = run_game(hand1, hand2)
     players_list = list_players(hand1, hand2)
-    if winner:
+    if winner == 404:
+        return render_template("bam.html")
+    elif winner:
         return render_template("winner.html", winner_string=f"{winner.name} wins by playing {winner.hand}", players=players_list)
     else:
         return render_template("winner.html", winner_string="It is a draw", players = players_list)
