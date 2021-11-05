0c5d8f8de1f0503270e6cb1886dac0a1ac8041c1
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-05 12:04:22 +0000

    adds init tests for Player

--- tests/player_test.py
+++ tests/player_test.py
@@ -2,4 +2,13 @@ import unittest
 from models.player import Player
 
 class TestPlayer(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.player = Player("Bob", "rock")
+
+    @unittest.skip('')
+    def test_player_has_name(self):
+        self.assertEqual("Bob", self.player.name)
+
+    @unittest.skip('')
+    def test_player_has_hand(self):
+        self.assertEqual("rock", self.player.hand)
