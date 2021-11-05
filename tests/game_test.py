b67007536d11101d8d1e1126595046a8e7cad087
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-05 12:39:33 +0000

    adds unit tests for play

--- tests/game_test.py
+++ tests/game_test.py
@@ -6,6 +6,7 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.player1 = Player("Bob", "rock")
         self.player2 = Player("Tom", "paper")
+        self.player3 = Player("Abe", "scissors")
 
     def test_game_has_player1(self):
         self.game = Game(self.player1, self.player2)
@@ -18,3 +19,38 @@ class TestGame(unittest.TestCase):
     def test_game_has_HANDS(self):
         self.game = Game(self.player1, self.player2)
         self.assertEqual({"rock": 0, "paper": 1, "scissors": 2}, self.game.HANDS)
+
+    @unittest.skip('')
+    def test_play_same_hands_returns_None(self):
+        self.game = Game(self.player1, self.player1)
+        self.assertEqual(None, self.game.play())
+
+    @unittest.skip('')
+    def test_play_player1_player2_returns_player2(self):
+        self.game = Game(self.player1, self.player2)
+        self.assertEqual(self.player2, self.game.play())
+    
+    @unittest.skip('')
+    def test_play_player2_player1_returns_player2(self):
+        self.game = Game(self.player2, self.player1)
+        self.assertEqual(self.player2, self.game.play())
+    
+    @unittest.skip('')
+    def test_play_player2_player3_returns_player3(self):
+        self.game = Game(self.player2, self.player3)
+        self.assertEqual(self.player3, self.game.play())
+    
+    @unittest.skip('')
+    def test_play_player3_player2_returns_player3(self):
+        self.game = Game(self.player3, self.player2)
+        self.assertEqual(self.player3, self.game.play())
+    
+    @unittest.skip('')
+    def test_play_player1_player3_returns_player1(self):
+        self.game = Game(self.player1, self.player3)
+        self.assertEqual(self.player1, self.game.play())
+    
+    @unittest.skip('')
+    def test_play_player3_player1_returns_player1(self):
+        self.game = Game(self.player3, self.player1)
+        self.assertEqual(self.player1, self.game.play())
