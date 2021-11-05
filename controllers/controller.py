51e717b1db25378a5da60d6942c732c25d48e5d7
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-05 14:56:25 +0000

    modifies /hand1/hand2 route to display both players

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,5 @@
 from app import app
-from models.run_game import run_game
+from models.run_game import run_game, list_players
 from flask import render_template
 
 @app.route('/')
@@ -9,7 +9,8 @@ def index():
 @app.route('/<hand1>/<hand2>')
 def rps(hand1, hand2):
     winner = run_game(hand1, hand2)
+    players_list = list_players(hand1, hand2)
     if winner:
-        return render_template("winner.html", winner_string=f"{winner.name} wins by playing {winner.hand}")
+        return render_template("winner.html", winner_string=f"{winner.name} wins by playing {winner.hand}", players=players_list)
     else:
-        return render_template("winner.html", winner_string="It is a draw")
+        return render_template("winner.html", winner_string="It is a draw", players = players_list)
