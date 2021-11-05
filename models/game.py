30675ec923faf0ae7c0d554df3e78f61aa8dbb13
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-05 17:59:21 +0000

    adds invalid_hand in Game

--- models/game.py
+++ models/game.py
@@ -5,7 +5,7 @@ class Game:
         self.HANDS = {"rock": 0, "paper": 1, "scissors": 2}
 
     def play(self):
-        if not self.player2.hand in self.HANDS or not self.player1.hand in self.HANDS:
+        if self.invalid_hand():
             return 404
         if self.player1.hand == self.player2.hand:
             return None
@@ -20,3 +20,7 @@ class Game:
 
     def hand(self, player):
         return self.HANDS[player.hand]
+
+    
+    def invalid_hand(self):
+        return not self.player1.hand in self.HANDS or not self.player2.hand in self.HANDS
