2a4b9f79dc31d7549bf2cc10235a10766fc7f2fa
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-05 12:21:42 +0000

    adds init tests for Game

--- tests/game_test.py
+++ tests/game_test.py
@@ -3,4 +3,21 @@ from models.game import Game
 from models.player import Player
 
 class TestGame(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.player1 = Player("Bob", "rock")
+        self.player2 = Player("Tom", "paper")
+
+    @unittest.skip('')
+    def test_game_has_player1(self):
+        self.game = Game(self.player1, self.player2)
+        self.assertEqual(self.player1, self.game.player1)
+
+    @unittest.skip('')
+    def test_game_has_player2(self):
+        self.game = Game(self.player1, self.player2)
+        self.assertEqual(self.player2, self.game.player2)
+
+    @unittest.skip('')
+    def test_game_has_HANDS(self):
+        self.game = Game(self.player1, self.player2)
+        self.assertEqual({"rock": 0, "paper": 1, "scissors": 2}, self.game.HANDS)
