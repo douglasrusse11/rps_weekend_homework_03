14c56d4d37091651b755c5e7273949137d7c2670
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-06 08:59:47 +0000

    modifies run_game, passes test

--- models/run_game.py
+++ models/run_game.py
@@ -1,9 +1,14 @@
 from models.player import Player
 from models.game import Game
 
-def run_game(hand1, hand2):
-    player1 = Player("Player 1", hand1)
-    player2 = Player("Player 2", hand2)
-    game = Game(player1, player2)
-    return game.play(), game.list_players()
+def run_game(input1, input2, computer=False):
+    if computer:
+        player1 = Player(input1, input2)
+        game = Game(player1)
+        return game.play_computer(), game.list_players()
+    else:
+        player1 = Player("Player 1", input1)
+        player2 = Player("Player 2", input2)
+        game = Game(player1, player2)
+        return game.play(), game.list_players()
 
