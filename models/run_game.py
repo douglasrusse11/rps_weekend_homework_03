d7b7ffbd9cb008cd21dba8d7376318dd75943d00
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-06 08:52:47 +0000

    modifies run_game

--- models/run_game.py
+++ models/run_game.py
@@ -5,5 +5,5 @@ def run_game(hand1, hand2):
     player1 = Player("Player 1", hand1)
     player2 = Player("Player 2", hand2)
     game = Game(player1, player2)
-    return game.play(), [player1, player2]
+    return game.play(), game.list_players()
 
