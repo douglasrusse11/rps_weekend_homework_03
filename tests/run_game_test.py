caccc332141205460816def17e1d7d99a20743c8
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-06 08:28:47 +0000

    updates tests for run_game using tuple assignment

--- tests/run_game_test.py
+++ tests/run_game_test.py
@@ -4,46 +4,51 @@ from models.run_game import run_game
 class TestRun_Game(unittest.TestCase):
 
     def test_run_game_rock_rock_returns_None(self):
-        self.assertEqual(None, run_game("rock", "rock"))
+        winner, players = run_game("rock", "rock")
+        self.assertEqual(None, winner)
     
     def test_run_game_paper_paper_returns_None(self):
-        self.assertEqual(None, run_game("paper", "paper"))
+        winner, players = run_game("paper", "paper")
+        self.assertEqual(None, winner)
     
     def test_run_game_scissors_scissors_returns_None(self):
-        self.assertEqual(None, run_game("scissors", "scissors"))
+        winner, players = run_game("scissors", "scissors")
+        self.assertEqual(None, winner)
     
     def test_run_game_rock_paper_returns_player2(self):
-        winner = run_game("rock", "paper")
+        winner, players = run_game("rock", "paper")
         self.assertEqual("Player 2", winner.name)
         self.assertEqual("paper", winner.hand)
     
     def test_run_game_paper_rock_returns_player1(self):
-        winner = run_game("paper", "rock")
+        winner, players = run_game("paper", "rock")
         self.assertEqual("Player 1", winner.name)
         self.assertEqual("paper", winner.hand)
 
     def test_run_game_paper_scissors_returns_player2(self):
-        winner = run_game("paper", "scissors")
+        winner, players = run_game("paper", "scissors")
         self.assertEqual("Player 2", winner.name)
         self.assertEqual("scissors", winner.hand)
 
     def test_run_game_scissors_paper_returns_player1(self):
-        winner = run_game("scissors", "paper")
+        winner, players = run_game("scissors", "paper")
         self.assertEqual("Player 1", winner.name)
         self.assertEqual("scissors", winner.hand)
 
     def test_run_game_scissors_rock_returns_player2(self):
-        winner = run_game("scissors", "rock")
+        winner, players = run_game("scissors", "rock")
         self.assertEqual("Player 2", winner.name)
         self.assertEqual("rock", winner.hand)
 
     def test_run_game_rock_scissors_returns_player1(self):
-        winner = run_game("rock", "scissors")
+        winner, players = run_game("rock", "scissors")
         self.assertEqual("Player 1", winner.name)
         self.assertEqual("rock", winner.hand)
 
     def test_run_game_rock_banana_returns_404(self):
-        self.assertEqual(404, run_game("rock", "banana"))
+        winner, players = run_game("rock", "banana")
+        self.assertEqual(404, winner)
 
     def test_run_game_dinosaur_paper_returns_404(self):
-        self.assertEqual(404, run_game("dinosaur", "paper"))
+        winner, players = run_game("dinosaur", "paper")
+        self.assertEqual(404, winner)
