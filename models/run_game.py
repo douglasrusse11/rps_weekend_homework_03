8f33a68a309dda692a6d7686995a2d9b02c81ed5
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-06 08:30:02 +0000

    modifies run_game, removes list_players

--- models/run_game.py
+++ models/run_game.py
@@ -5,10 +5,5 @@ def run_game(hand1, hand2):
     player1 = Player("Player 1", hand1)
     player2 = Player("Player 2", hand2)
     game = Game(player1, player2)
-    return game.play()
+    return game.play(), [player1, player2]
 
-def list_players(hand1, hand2):
-    player1 = Player("Player 1", hand1)
-    player2 = Player("Player 2", hand2)
-    players = [player1, player2]
-    return players
